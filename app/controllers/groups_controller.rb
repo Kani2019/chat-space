d3ac6ba0f375d7983730d10ce0e260dd77d0b010
Kani2019 <seiya.k.w@icloud.com>
2019-12-04 00:58:33 +0900

    updeta-edit

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -3,10 +3,12 @@ class GroupsController < ApplicationController
 
   def index
   end
+
   def new
     @group = Group.new
     @group.users << current_user
   end
+
   def create
     @group = Group.new(group_params)
     if @group.save
@@ -15,20 +17,22 @@ class GroupsController < ApplicationController
       render :new
     end
   end
-  def edit
-  end
+
   def update
     if @group.update(group_params)
-      redirect_to group_messages_path(@group), notice: "グループを編集しました。"
+      redirect_to root_path, notice: 'グループを更新しました'
     else
       render :edit
     end
   end
+
   private
   def group_params
-    params.require(:group).permit(:name, user_ids: [])
+    params.require(:group).permit(:name, user_ids: [] )
   end
+
   def set_group
     @group = Group.find(params[:id])
   end
+  
 end
